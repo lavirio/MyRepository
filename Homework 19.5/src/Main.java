cf6d82a88f922cb5f7625134d5869847addd643d
lavirio <convers11@me.com>
2021-09-01 22:38:43 +0400

    Done with рекурсия

--- Homework 19.5/src/Main.java
+++ Homework 19.5/src/Main.java
@@ -15,18 +15,16 @@ public class Main {
         return new Scanner(System.in).next().charAt(0);
     }
     public static int calc(int num1, int num2, char operation) {
-        int result = 0;
-        switch(operation) {
-            case '+': result = num1 + num2;
-            break;
-            case '-': result = num1 - num2;
-            break;
-            case '*': result = num1 * num2;
-            break;
-            case '/': result = num1 / num2;
-            break;
-            default:
-                System.out.println("Ошибка");
+        int result;
+        switch (operation) {
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> result = num1 / num2;
+            default -> {
+                System.out.println("Операция не распознана. Повторите ввод");
+                result = calc(num1, num2, getOperation());
+            }
         }
         return result;
     }
