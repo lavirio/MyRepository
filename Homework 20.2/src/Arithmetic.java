4d749ccedf8ee2fd0d805df6aa6e612f6d89509d
lavirio <convers11@me.com>
2021-09-03 21:25:00 +0400

    Version 3

--- Homework 20.2/src/Arithmetic.java
+++ Homework 20.2/src/Arithmetic.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 public class Arithmetic {
     int num1;
@@ -8,29 +7,14 @@ public class Arithmetic {
         this.num1 = num1;
         this.num2 = num2;
     }
-    public int getInt(){
-        System.out.print("Введите число: ");
-        return new Scanner(System.in).nextInt();
-    }
-    public void calc(int num1, int num2) {
-        int sum;
-        int diff;
-        int inc;
-        int avg;
-        int min;
-        int max;
-        diff = num1 - num2;
-        sum = num1 + num2;
-        inc = num1 * num2;
-        avg = (num1 + num2)/2;
-        min = Math.min(num1, num2);
-        max = Math.max(num1, num2);
-        System.out.print("Сумма двух чисел: " + sum + "\n");
-        System.out.print("Разность двух чисел: " + diff + "\n");
-        System.out.print("Произведение двух чисел: " + inc + "\n");
-        System.out.print("Разность двух чисел: " + avg + "\n");
-        System.out.print("Минимум из двух чисел: " + min + "\n");
-        System.out.print("Максимум из двух чисел: " + max + "\n");
+
+    public void calc() {
+        System.out.print("Сумма двух чисел: " + (num1 + num2) + "\n");
+        System.out.print("Разность двух чисел: " + (num1 - num2) + "\n");
+        System.out.print("Произведение двух чисел: " + num1 * num2 + "\n");
+        System.out.print("Среднее значение двух чисел: " + (num1 + num2)/2 + "\n");
+        System.out.print("Минимум из двух чисел: " + Math.min(num1,num2) + "\n");
+        System.out.print("Максимум из двух чисел: " + Math.max(num1,num2) + "\n");
     }
 
 
--- Homework 20.2/src/Main.java
+++ Homework 20.2/src/Main.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Arithmetic arith = new Arithmetic(1,2);
-        arith.calc(arith.getInt(), arith.getInt());
+        Arithmetic arith = new Arithmetic(getInt(),getInt());
+        arith.calc();
+    }
+    public static int getInt(){
+        System.out.print("Введите число: ");
+        return new Scanner(System.in).nextInt();
     }
 }
