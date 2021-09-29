99b93b405e23149329eb50eb1d41ef6c38a0c57e
lavirio <convers11@me.com>
2021-09-29 15:18:16 +0300

    Version 1

---
+++ Homework 22.1/src/Country.java
@@ -0,0 +1,51 @@
+public class Country {
+    private String name;
+    private int quantity;
+    private int land;
+    private String capital;
+    private char sea;
+
+    public Country(String capital) {
+        this.capital = capital;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getLand() {
+        return land;
+    }
+
+    public void setLand(int land) {
+        this.land = land;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public char getSea() {
+        return sea;
+    }
+
+    public void setSea(char sea) {
+        this.sea = sea;
+    }
+}
