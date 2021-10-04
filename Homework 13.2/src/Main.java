8b951df6ecd11726381ed31d6f42ac568bacd549
lavirio <convers11@me.com>
2021-10-04 18:00:52 +0300

    Version 2

--- Homework 13.2/src/Main.java
+++ Homework 13.2/src/Main.java
@@ -11,13 +11,20 @@ public class Main {
             for (int a = 0; a < name.length(); a++) {
                 if (name.charAt(a) == ' ') {
                     space++;
+                    break;
                 }
             }
             for (int b = 0; b < name.length(); b++) {
                 if (name.charAt(b) >= '0' && name.charAt(b) <= '9' || space > 2 || space == 0) {
-                    System.out.println("Ввделеная строка не является ФИО!");
+                    System.out.println("Введеная строка не является ФИО!");
                     break;
-                } else {
+                } else if (name.charAt(b) >= '0' && name.charAt(b) <= '9' || space == 1) {
+                    System.out.println("Фамилия: " + name.substring(0, name.indexOf(' ')));
+                    System.out.println("Имя: " + name.substring(name.lastIndexOf(' ')));
+                    System.out.println("Отчество: Нет");
+                    break;
+                }
+                else {
                     System.out.println("Фамилия: " + name.substring(0, name.indexOf(' ')));
                     System.out.println("Имя: " + name.substring(name.indexOf(' '), name.lastIndexOf(' ')));
                     System.out.println("Отчество: " + name.substring(name.lastIndexOf(' ')));
