a1a3557dae5101d1a0811a17bcd64fbe9811ce3f
lavirio <convers11@me.com>
2021-10-04 16:18:13 +0300

    Version 5

--- Hotel/src/Hotel.java
+++ Hotel/src/Hotel.java
@@ -1,81 +1,82 @@
 import java.util.Scanner;
 
 public class Hotel {
-    private final int lobby = 0;
-    private int mainFloor = 0;
-    private final int bar = 7;
-    private final int guestPermissionAge = 18;
-    private final int guestAdolescenceAge = 14;
-    private final int guestYoungAge = 12;
+    private final int LOBBY = 0;
+    private int MAINFLOOR = 0;
+    private final int BAR = 7;
+    private final int GUESTPERMISSIONAGE = 18;
+    private final int GUESTADOLESENCEAGE = 14;
+    private final int GUESTYOUNGAGE = 12;
     private boolean isContinue1 = true;
     private boolean isContinue2 = true;
     private boolean isContinue3 = true;
 
     public void goUp(Guest guest) {
         while (isContinue1) {
-            if(mainFloor == bar) {
-                System.out.println("Вы поднялись на максимальный этаж № " + mainFloor); return;}
+            if(MAINFLOOR == BAR) {
+                System.out.println("Вы поднялись на максимальный этаж № " + MAINFLOOR); return;}
             System.out.println("Подняться выше или спуститься ниже? (Up/Down)");
             Scanner scanner = new Scanner(System.in);
             if(scanner.nextLine().equals( "Down")) {
                 isContinue1 = false;
                 return;
             }
-            if (guest.getGuestAge() >= guestPermissionAge) {if (mainFloor < bar ){
-                mainFloor = mainFloor + 1;
-                System.out.println("Вы поднялись на этаж № " + mainFloor);
+            if (guest.getGuestAge() >= GUESTPERMISSIONAGE) {if (MAINFLOOR < BAR){
+                MAINFLOOR = MAINFLOOR + 1;
+                System.out.println("Вы поднялись на этаж № " + MAINFLOOR);
             }
-            else {System.out.println("Вы поднялись на максимальный этаж № " + mainFloor); return;}}
-            else if (guest.getGuestAge() < guestPermissionAge && guest.getGuestAge() >= guestAdolescenceAge) {
+            else {System.out.println("Вы поднялись на максимальный этаж № " + MAINFLOOR); return;}}
+            else if (guest.getGuestAge() < GUESTPERMISSIONAGE && guest.getGuestAge() >= GUESTADOLESENCEAGE) {
                 int pool = 6;
-                if (mainFloor < pool) {
-                mainFloor = mainFloor + 1;
-                System.out.println("Вы поднялись на этаж № " + mainFloor);
+                if (MAINFLOOR < pool) {
+                MAINFLOOR = MAINFLOOR + 1;
+                System.out.println("Вы поднялись на этаж № " + MAINFLOOR);
             }
-            else {System.out.println("Вы поднялись на максимально разрешенный для Вас этаж № " + mainFloor); return;}}
-            else if (guest.getGuestAge() < guestAdolescenceAge && guest.getGuestAge() >= guestYoungAge) {
+            else {System.out.println("Вы поднялись на максимально разрешенный для Вас этаж № " + MAINFLOOR); return;}}
+            else if (guest.getGuestAge() < GUESTADOLESENCEAGE && guest.getGuestAge() >= GUESTYOUNGAGE) {
                 int maxFloor = 5;
-                if (mainFloor < maxFloor){
-                mainFloor = mainFloor + 1;
-                System.out.println("Вы поднялись на этаж № " + mainFloor);}
-            else {System.out.println("Вы поднялись на максимально разрешенный для Вас этаж № " + mainFloor); return;}}
+                if (MAINFLOOR < maxFloor){
+                MAINFLOOR = MAINFLOOR + 1;
+                System.out.println("Вы поднялись на этаж № " + MAINFLOOR);}
+            else {System.out.println("Вы поднялись на максимально разрешенный для Вас этаж № " + MAINFLOOR); return;}}
             else {
                 System.out.println("Вам запрещено заселяться в отель!");
             }
         }
     }
     public void goDown(Guest guest) {
-        if(mainFloor != bar && mainFloor != lobby) {mainFloor = mainFloor - 1;
-            System.out.println("Вы спустились на этаж № " + mainFloor);}
+        if(MAINFLOOR != BAR && MAINFLOOR != LOBBY) {
+            MAINFLOOR = MAINFLOOR - 1;
+            System.out.println("Вы спустились на этаж № " + MAINFLOOR);}
         while (isContinue2) {
-            if(mainFloor == lobby) {return;}
+            if(MAINFLOOR == LOBBY) {return;}
             System.out.println("Спуститься ниже или остаться на этом этаже? (Down/Stay)");
             Scanner scanner = new Scanner(System.in);
             if(scanner.nextLine().equals("Stay")) {
                 isContinue2 = false;
-                System.out.println("Вы остались на этаже № " + mainFloor);
+                System.out.println("Вы остались на этаже № " + MAINFLOOR);
                 return;
             }
-            if (guest.getGuestAge() >= guestPermissionAge) {if(mainFloor != lobby) {
-                mainFloor = mainFloor > lobby ? mainFloor - 1 : mainFloor;
-                System.out.println("Вы спустились на этаж № " + mainFloor);}
-            else {System.out.println("Вы спустились на минимальный этаж № " + mainFloor); return;}}
-            else if (guest.getGuestAge() < guestPermissionAge && guest.getGuestAge() >= guestAdolescenceAge) {if(mainFloor != lobby){
-                mainFloor = mainFloor > lobby ? mainFloor - 1 : mainFloor;
-                System.out.println("Вы спустились на этаж № " + mainFloor);}
-            else {System.out.println("Вы спустились на минимальный этаж № " + mainFloor); return;}}
-            else if (guest.getGuestAge() < guestAdolescenceAge && guest.getGuestAge() >= guestYoungAge) {if(mainFloor != lobby) {
-                mainFloor = mainFloor > lobby ? mainFloor - 1 : mainFloor;
-                System.out.println("Вы спустились на этаж № " + mainFloor);}
-            else {System.out.println("Вы спустились на минимальный этаж № " + mainFloor); return;}}
+            if (guest.getGuestAge() >= GUESTPERMISSIONAGE) {if(MAINFLOOR != LOBBY) {
+                MAINFLOOR = MAINFLOOR > LOBBY ? MAINFLOOR - 1 : MAINFLOOR;
+                System.out.println("Вы спустились на этаж № " + MAINFLOOR);}
+            else {System.out.println("Вы спустились на минимальный этаж № " + MAINFLOOR); return;}}
+            else if (guest.getGuestAge() < GUESTPERMISSIONAGE && guest.getGuestAge() >= GUESTADOLESENCEAGE) {if(MAINFLOOR != LOBBY){
+                MAINFLOOR = MAINFLOOR > LOBBY ? MAINFLOOR - 1 : MAINFLOOR;
+                System.out.println("Вы спустились на этаж № " + MAINFLOOR);}
+            else {System.out.println("Вы спустились на минимальный этаж № " + MAINFLOOR); return;}}
+            else if (guest.getGuestAge() < GUESTADOLESENCEAGE && guest.getGuestAge() >= GUESTYOUNGAGE) {if(MAINFLOOR != LOBBY) {
+                MAINFLOOR = MAINFLOOR > LOBBY ? MAINFLOOR - 1 : MAINFLOOR;
+                System.out.println("Вы спустились на этаж № " + MAINFLOOR);}
+            else {System.out.println("Вы спустились на минимальный этаж № " + MAINFLOOR); return;}}
         }
     }
     public void goOut(Guest guest) {
-        if (guest.getGuestAge() < guestAdolescenceAge) {
+        if (guest.getGuestAge() < GUESTADOLESENCEAGE) {
             System.out.println("Вам запрешено покидать отель в одиночку!");
         } else {
             while (isContinue3) {
-                if (mainFloor != lobby) {
+                if (MAINFLOOR != LOBBY) {
                     isContinue3 = false;
                 } else {
                     System.out.println("Выйти из отеля или остаться? (Out/Stay)");
@@ -92,13 +93,13 @@ public class Hotel {
         }
     }
     public void info(Guest guest) {
-        if(guest.getGuestAge() >= guestPermissionAge) {
+        if(guest.getGuestAge() >= GUESTPERMISSIONAGE) {
             System.out.println("Вам разрешено посещать все этажи отеля!");
         }
-        else if (guest.getGuestAge() < guestPermissionAge && guest.getGuestAge() >= guestAdolescenceAge) {
+        else if (guest.getGuestAge() < GUESTPERMISSIONAGE && guest.getGuestAge() >= GUESTADOLESENCEAGE) {
             System.out.println("Вам запрещено подниматься выше 6 этажа!");
         }
-        else if (guest.getGuestAge() < guestAdolescenceAge && guest.getGuestAge() >= guestYoungAge) {
+        else if (guest.getGuestAge() < GUESTADOLESENCEAGE && guest.getGuestAge() >= GUESTYOUNGAGE) {
             System.out.println("Вам запрещено подниматься выше 5 этажа!");
         }
         else {
