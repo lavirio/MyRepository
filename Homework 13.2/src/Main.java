96aeb54603862252ac295605aabd8a781727abe4
lavirio <convers11@me.com>
2021-10-04 17:42:25 +0300

    Version 1

---
+++ Homework 13.2/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int space = 0;
+        boolean isContinue = true;
+
+        while (isContinue) {
+        System.out.print("Введите ФИО: ");
+        String name = new Scanner(System.in).nextLine();
+            for (int a = 0; a < name.length(); a++) {
+                if (name.charAt(a) == ' ') {
+                    space++;
+                }
+            }
+            for (int b = 0; b < name.length(); b++) {
+                if (name.charAt(b) >= '0' && name.charAt(b) <= '9' || space > 2 || space == 0) {
+                    System.out.println("Ввделеная строка не является ФИО!");
+                    break;
+                } else {
+                    System.out.println("Фамилия: " + name.substring(0, name.indexOf(' ')));
+                    System.out.println("Имя: " + name.substring(name.indexOf(' '), name.lastIndexOf(' ')));
+                    System.out.println("Отчество: " + name.substring(name.lastIndexOf(' ')));
+                    break;
+                }
+            }
+            System.out.println("Хотите попробовать снова? (Y/N)");
+            Scanner scanner = new Scanner(System.in);
+            if(scanner.nextLine().equals("N")) {
+                isContinue = false;
+            }
+        }
+    }
+}
