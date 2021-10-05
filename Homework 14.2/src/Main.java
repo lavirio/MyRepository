0dfc4a0fe0d587509f90c79592b176a55c27124d
lavirio <convers11@me.com>
2021-10-05 17:30:04 +0300

    Version 2

--- Homework 14.2/src/Main.java
+++ Homework 14.2/src/Main.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getText().replaceAll("[А-я]+"+".", " ").trim().replaceAll("\\d+", " ").trim());
-
+        System.out.println(getText().replaceAll("[А-я]+"+".", " ").replaceAll("\\d+", " ").trim());
     }
 
     public static  String getText(){
