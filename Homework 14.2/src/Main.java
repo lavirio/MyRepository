cfaa97d197b1db7c41195f1de2a8fdd392b6cfdd
lavirio <convers11@me.com>
2021-10-05 17:28:48 +0300

    Version 1

---
+++ Homework 14.2/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getText().replaceAll("[А-я]+"+".", " ").trim().replaceAll("\\d+", " ").trim());
+
+    }
+
+    public static  String getText(){
+        System.out.println("Введите текст на английском языке: ");
+        String str = new Scanner(System.in).nextLine();
+        return str;
+    }
+}
