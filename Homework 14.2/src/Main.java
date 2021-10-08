7384764bf3610c525a759f8d7ae60b7f5c541dc4
lavirio <convers11@me.com>
2021-10-08 10:13:13 +0300

    Version 6

--- Homework 14.2/src/Main.java
+++ Homework 14.2/src/Main.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getText().replaceAll("\\W+", " ").trim().replaceAll("\\d+", " ").trim().replaceAll("  ", " "));
+        System.out.println(getText().replaceAll("\\W+"," ").replaceAll("\\d+", " ").replaceAll("\\s+", " ").trim().replaceAll(" ","\n"));
     }
 
     public static  String getText(){
         System.out.println("Введите текст на английском языке: ");
         return new Scanner(System.in).nextLine();
-
     }
 }
