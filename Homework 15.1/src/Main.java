9405d5f71111faad2fa113b40077b627085ddb92
lavirio <convers11@me.com>
2021-10-18 18:32:10 +0300

    Version 2

--- Homework 15.1/src/Main.java
+++ Homework 15.1/src/Main.java
@@ -1,28 +1,52 @@
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите дату ваше рождения (день/месяц/год)");
-        String birthdayDate = new Scanner(System.in).nextLine().replaceAll("\\D+", ".");
+        LocalDate current = LocalDate.now();
 
-        LocalDateTime localDateTime = LocalDateTime.now();
-        DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        localDateTime.atOffset((ZoneOffset) dateTimeFormatter1.parse(birthdayDate));
+        System.out.println("Введите год вашего рождения?");
+        int year = new Scanner(System.in).nextInt();
 
-        LocalDateTime localDateTime1 = LocalDateTime.now();
-        DateTimeFormatter dateTimeFormatter2 = DateTimeFormatter.ofPattern("dd.MM.yyyy - eee");
+        System.out.println("Введите месяц вашего рождения?");
+        int month = new Scanner(System.in).nextInt();
 
-        if (localDateTime1.isBefore(localDateTime)) {
-            System.out.println("Введеная дата еще не наступила!");
-        } else {
-            while (localDateTime.isBefore(localDateTime1)) {
-                System.out.println(dateTimeFormatter2.format(localDateTime));
-                localDateTime.plusYears(1);
-            }
+        System.out.println("Введите число вашего рождения?");
+        int day = new Scanner(System.in).nextInt();
 
+        LocalDate burn = LocalDate.of(year,month,day);
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - eee");
+
+//        System.out.println(dateTimeFormatter.format(burn));
+//        System.out.println(dateTimeFormatter.format(current));
+
+        if(current.isBefore(burn)) {
+            System.out.println("Введеная дата еще не наступила");
         }
+        else {
+            while (burn.isBefore(current)) {
+                System.out.println(dateTimeFormatter.format(burn));
+                burn.plusYears(1);
+            }
+        }
+
+
+//        LocalDateTime localDateTime = LocalDateTime.now();
+//        DateTimeFormatter dateTimeFormatter1 = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+//        localDateTime.atOffset((ZoneOffset) dateTimeFormatter1.parse(birthdayDate));
+//
+//        LocalDateTime localDateTime1 = LocalDateTime.now();
+//        DateTimeFormatter dateTimeFormatter2 = DateTimeFormatter.ofPattern("dd.MM.yyyy - eee");
+//
+//        if (localDateTime1.isBefore(localDateTime)) {
+//            System.out.println("Введеная дата еще не наступила!");
+//        } else {
+//            while (localDateTime.isBefore(localDateTime1)) {
+//                System.out.println(dateTimeFormatter2.format(localDateTime));
+//                localDateTime.plusYears(1);
+//            }
+//        }
     }
 }
