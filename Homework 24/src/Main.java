1ff68d380ad9672a91fd62094d1d39b457d0d397
lavirio <convers11@me.com>
2021-10-26 23:27:14 +0300

    Version 2

--- Homework 24/src/Main.java
+++ Homework 24/src/Main.java
@@ -4,61 +4,60 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в симулятор записной книжки.\n" +
                 "Здесь можно выполнять разные действия, а именно:\n" +
-                "1)Put - добавлять новые контакты\n" +
-                "2)Delete - удалять контакт\n" +
-                "3)Clear - очистить записную книжку\n" +
-                "4)Search name - поиск контактной информации по имени\n" +
-                "5)Search number - поиск контакт информации по номеру телефона\n" +
-                "6)All - распечатать все контакты из записной книжки\n" +
-                "7)Info - посмотреть доступные команды\n" +
-                "8)Exit - выход из программы");
+                "1)Добавлять новые контакты\n" +
+                "2)Поиск контактной информации по имени\n" +
+                "3)Поиск контакт информации по номеру телефона\n" +
+                "4)Clear - очистить записную книжку\n" +
+                "5)List - распечатать все контакты из записной книжки\n" +
+                "6)Info - посмотреть доступные команды\n" +
+                "7)Exit - выход из программы");
 
         while (true) {
-            System.out.println("Введите команду:");
+            System.out.println("Введите  номер, имя или команду:");
             String action = new Scanner(System.in).nextLine();
 
-            if (action.toLowerCase().startsWith("put")) {
-                put(action);
-            } else if (action.toLowerCase().startsWith("delete")) {
-                delete(action);
-            } else if (action.toLowerCase().startsWith("clear")) {
+            if (action.toLowerCase().startsWith("clear")) {
                 clear();
-            } else if (action.toLowerCase().matches("\\S+\\s+\\S+")) {
-                searchName(action);
-            } else if (action.toLowerCase().matches("\\S+\\s+\\d+")) {
-                searchNumber(action);
-            } else if (action.toLowerCase().startsWith("all")) {
+            } else if (action.toLowerCase().startsWith("list")) {
                 print();
             } else if (action.toLowerCase().startsWith("info")) {
                 info();
             } else if (action.toLowerCase().startsWith("exit")) {
                 System.out.println("Вы вышли из программы!");
                 return;
+            } else if (action.toLowerCase().matches("^\\d+")) {
+                searchNumber(action);
+            } else if (action.toLowerCase().matches("(((.+){1,999})?)")) {
+                searchName(action);
             } else {
                 System.out.println("Команда не распознана, повторите ввод!");
             }
         }
     }
 
-    private static void put(String action) {
-        PhoneBook.putNewContact(getName(action), getNumber(action));
-    }
-
-    private static void delete(String action) {
-        PhoneBook.deleteContact(getName(action));
-    }
-
     private static void clear() {
         PhoneBook.clearAllContacts();
     }
 
-    private static void searchName(String action) {
-        PhoneBook.searchContactsByName(getName(action));
+    private static void searchNumber(String action) {
+        if (action.matches("(7)[0-9]{10}")) {
+            String matchNumber = action.replaceAll("^7", "+7");
+            PhoneBook.searchContactsByNumber(matchNumber);
+        } else if (action.matches("(8)[0-9]{10}")) {
+            String matchNumber = action.replaceAll("^8", "+7");
+            PhoneBook.searchContactsByNumber(matchNumber);
+        } else if (action.matches("[0-9]{10}")) {
+            String matchNumber = "+7".concat(action);
+            PhoneBook.searchContactsByNumber(matchNumber);
+        } else {
+            System.out.println("Вы ввели номер неправльного формата, повторите ввод:");
+        }
     }
 
-    private static void searchNumber(String action) {
-        PhoneBook.searchContactsByNumber(getNumber(action));
+    private static void searchName(String action) {
+        PhoneBook.searchContactsByNameConditions(action);
     }
+
 
     private static void print() {
         PhoneBook.printAllContacts();
@@ -66,21 +65,5 @@ public class Main {
 
     private static void info() {
         PhoneBook.directoryInfo();
-    }
-
-    public static String getName(String action) {
-        if (action.matches("\\S+\\s+\\S+")) {
-            return action.split("\\s+", 2)[1].trim();
-        } else {
-            return action.split("\\s+", 3)[1].trim();
-        }
-    }
-
-    public static String getNumber(String action) {
-        if (action.matches("\\S+\\s+\\d+")) {
-            return action.split("\\s+", 2)[1].trim();
-        } else {
-            return action.split("\\s+", 3)[2].trim();
-        }
     }
 }
--- Homework 24/src/PhoneBook.java
+++ Homework 24/src/PhoneBook.java
@@ -1,45 +1,58 @@
 import java.util.Map;
+import java.util.Scanner;
 import java.util.TreeMap;
 
 public class PhoneBook {
     private static final Map<String, String> phoneBook = new TreeMap<>();
 
-    public static void putNewContact(String name, String number) {
-        phoneBook.put(name, number);
-        System.out.println("Контакт с именем " + name + " добавлен");
-    }
-
-    public static void deleteContact(String name) {
-        if (!phoneBook.containsKey(name)) {
-            System.out.println("Контакта с таким именем нет!");
+    public static void clearAllContacts() {
+        if (phoneBook.isEmpty()) {
+            System.out.println("Невозможно выполнить действие так как записная книжка пуста!");
             return;
         }
-        phoneBook.remove(name);
-        System.out.println("Контакт " + name + " успешно удален");
-    }
-
-    public static void clearAllContacts() {
         phoneBook.clear();
         System.out.println("Все контакты успешно удалены!");
     }
 
-    public static void searchContactsByName(String name) {
-        if (!phoneBook.containsKey(name)) {
-            System.out.println("Контакта с таким именем нет в записной книжке!");
-            return;
+    public static void searchContactsByNameConditions(String name) {
+        if (phoneBook.containsKey(name)) {
+            System.out.println("Контакт " + name + " с  номером телефона " + phoneBook.get(name));
+        } else {
+            System.out.println("Контакта с таким именем нет в записной книжке, чтобы его добавть введите номер телефона:");
+            searchContactsByName(name);
         }
-        System.out.println(name + " " + phoneBook.get(name));
+    }
+
+    public static void searchContactsByName(String name) {
+        String number = new Scanner(System.in).nextLine();
+        if (number.matches("(7)[0-9]{10}")) {
+            phoneBook.put(name, "+" + number);
+            System.out.println("Контакт " + name + " с  номером телефона " + phoneBook.get(name) + " успешно добавлен!");
+        } else if (number.matches("(8)[0-9]{10}")) {
+            phoneBook.put(name, number.replaceAll("^8", "+7"));
+            System.out.println("Контакт " + name + " с  номером телефона " + phoneBook.get(name) + " успешно добавлен!");
+        } else if (number.matches("[0-9]{10}")) {
+            phoneBook.put(name, "+7" + number);
+            System.out.println("Контакт " + name + " с  номером телефона " + phoneBook.get(name) + " успешно добавлен!");
+        } else {
+            System.out.println("Номер неправильного формата, повторите ввод: ");
+            searchContactsByName(name);
+        }
     }
 
     public static void searchContactsByNumber(String number) {
-        if (!phoneBook.containsValue(number)) {
-            System.out.println("Контакта с таким номером не существует!");
-            return;
-        }
-        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
-            if (entry.getValue().equals(number)) {
-                System.out.println(entry.getKey() + " " + entry.getValue());
+        if (phoneBook.containsValue(number)) {
+            for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
+                if (entry.getValue().equals(number)) {
+                    System.out.println("Контакт " + entry.getKey() + " с номером телефона  " + entry.getValue());
+                }
             }
+        } else {
+            System.out.println("Контакта с таким номером нет в записной книжке, чтобы его добавить введите имя:");
+            String name = new Scanner(System.in).nextLine();
+            phoneBook.put(name, number);
+            System.out.println("Контакт " + name + " с  номером телефона " + phoneBook.get(name) + " успешно добавлен!");
+
         }
     }
 
@@ -54,13 +67,12 @@ public class PhoneBook {
     }
 
     public static void directoryInfo() {
-        System.out.println("1)Put - добавлять новые контакты\n" +
-                "2)Delete - удалять контакт\n" +
-                "3)Clear - очистить записную книжку\n" +
-                "4)Search name - поиск контактной информации по имени\n" +
-                "5)Search number - поиск контакт информации по номеру телефона\n" +
-                "6)All - распечатать все контакты из записной книжки\n" +
-                "7)Info - посмотреть доступные команды\n" +
-                "8)Exit - выход из программы");
+        System.out.println("1)Добавлять новые контакты\n" +
+                "2)Поиск контактной информации по имени\n" +
+                "3)Поиск контакт информации по номеру телефона\n" +
+                "4)Clear - очистить записную книжку\n" +
+                "5)List - распечатать все контакты из записной книжки\n" +
+                "6)Info - посмотреть доступные команды\n" +
+                "7)Exit - выход из программы");
     }
 }
